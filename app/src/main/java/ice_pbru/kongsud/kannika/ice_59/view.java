bef493a213225a979fab1627cb00a271e1d9e7b8
KannikaTOEY <toeymickey@hotmail.com>
2016-04-07 17:00:59 +0700

    Diodes3

--- app/src/main/java/ice_pbru/kongsud/kannika/ice_59/view.java
+++ app/src/main/java/ice_pbru/kongsud/kannika/ice_59/view.java
@@ -1,10 +1,13 @@
 package ice_pbru.kongsud.kannika.ice_59;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by ICE_PBRU on 4/7/2016.
@@ -37,6 +40,17 @@ public class view extends BaseAdapter{
 }
         @Override
         public View getView ( int i,View view,ViewGroup viewGroup){
-            return null;
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view1= layoutInflater.inflate(R.layout.list_view, viewGroup, false);
+//for icon
+            ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+            iconImageView.setImageResource(iconInt[i]);
+//for title
+            TextView titTextView = (TextView) view1.findViewById(R.id.textView2);
+            titTextView.setText(titleStrings[i]);
+//for detail
+            TextView detailTextView = (TextView) view1.findViewById(R.id.textView3);
+            detailTextView.setText(detailStrings[i]);
+            return view1;
         }
     }
